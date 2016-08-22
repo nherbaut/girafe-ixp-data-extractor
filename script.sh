3161aaba1a0de1f36432f1d7b1afe1c9145c5f50
Nicolas Herbaut <nherbaut@labri.fr>
2016-08-22 14:17:55 +0000

    fixed cron

--- script.sh
+++ script.sh
@@ -1,19 +1,24 @@
-ROOT=.
-rm *.csvx
-cat $1 |sed -rn 's/^(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O \1-$(date "+%Y-%m-%dT%H:%M:%S").png -q/p' | source /dev/stdin
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo -e "\e[32mremoving old csvx file\e[39m"
+rm $DIR/*.csvx 2> /dev/null
+echo -e "\e[32mdownloading new png files\e[39m"
+cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O $DIR\/\1--$(date "+%Y-%m-%dT%H:%M:%S").png /p' | source /dev/stdin
 
-for t in `ls *.png | sed -r "s/^([a-z]{1,2}-[a-z0-9]{1,5}_[0-9][wdWD])-.*$/\1/p"|uniq`; do
-scaleY=$(cat $1|grep $t|sed -r "s/.*,(.*)$/\1/g")
+echo -e "\e[32mfor each config key"
+for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/\1/p'`; do
+   echo -e "\e[32mworking with $t\e[39m"
+   scaleY=$(cat $1|grep $t|sed -r "s/.*,(.*),.*$/\1/g")
+   period=$(cat $1|grep $t|sed -r "s/.*,(.*)$/\1/g")
+   echo -e "\e[32mscale = $scaleY         period = $period\e[39m"
    for i in `ls $t*.png`; do
-   adate=$(ls $i|sed -r "s/^[a-z]{1,2}-[a-z0-9]{1,5}_[0-9][wdWD]-(.*)\.png/\1/g")
-   period=$(ls $i|sed -r "s/^[a-z]{1,2}-[a-z0-9]{1,5}_([0-9][wdWD])-.*\.png/\1/g")
-   ./franceix-data-extractor.py --file $i --date $adate --maxY $scaleY -p $period>> $t.csvx
-   echo "extracted $i"
+      echo -e "\e[32mextracting $i\e[39m"   
+      adate=$(ls $i|sed -r "s/^.*--(.*)\.png/\1/g")
+      $DIR/franceix-data-extractor.py --file $i --date $adate --maxY $scaleY -p $period>> $t.csvx
    done
-   echo "consolidating $t"
-   ./consolidate-capture.py -f ${t}.csvx -r 10T -o ${t}_10T.csvx
-   ./consolidate-capture.py -f ${t}.csvx -r 1H -o ${t}_1H.csvx
-   echo "consolidated $t"
+   echo -e "\e[32mconsolidating $t\e[39m"
+   $DIR/consolidate-capture.py -f ${t}.csvx -r 10T -o ${t}_10T.csvx
+   $DIR/consolidate-capture.py -f ${t}.csvx -r 1H -o ${t}_1H.csvx
+   echo -e "\e[32mconsolidated $t\e[39m"
 
 done
 
