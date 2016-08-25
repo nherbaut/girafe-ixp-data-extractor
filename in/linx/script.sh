51b32c48f193e0e2766957c43008350aca559b77
dbourasseau <dbourasseau@viotech.net>
2016-08-25 12:10:30 +0200

    finish extracted linx

--- in/linx/script.sh
+++ in/linx/script.sh
@@ -2,10 +2,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "\e[32mremoving old csvx file\e[39m"
 rm $DIR/*.csvx 2> /dev/null
 echo -e "\e[32mdownloading new json files\e[39m"
-cat $1 |sed -rn 's/^(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O $DIR\/\1--$(date "+%Y-%m-%dT%H:%M:%S").json /p' | source /dev/stdin
+cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O $DIR\/\1--$(date "+%Y-%m-%dT%H:%M:%S").json /p' | source /dev/stdin
 
 echo -e "\e[32mfor each config key"
-for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*)$/\1/p'`; do
+for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/\1/p'`; do
    echo -e "\e[32mworking with $t\e[39m"
    #scaleY=$(cat $1|grep $t|sed -r "s/.*,(.*),.*$/\1/g")
    period=$(cat $1|grep $t|sed -r "s/.*,(.*)$/\1/g")
@@ -15,13 +15,19 @@ for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*)$/\1/p'`; do
       #adate=$(ls $i|sed -r "s/^.*--(.*)\.json/\1/g")
       
       for j in `cat $2 |sed -r "s/,.*//"`; do
-		echo $j-$t.csvx
-		$DIR/linx-data-extractor.py --file $i --ixp $j>> $j-$t.csvx
+		echo $t-$j.csvx
+		$DIR/linx-data-extractor.py --file $i --ixp $j>> $t-$j.csvx
       done
    done
-  # echo -e "\e[32mconsolidating $t\e[39m"
-   #$DIR/consolidate-capture.py -f ${t}.csvx -r 10T -o ${t}_10T.csvx
-   #$DIR/consolidate-capture.py -f ${t}.csvx -r 1H -o ${t}_1H.csvx
-   #echo -e "\e[32mconsolidated $t\e[39m"
+   
+   resolution=$(cat $1|grep $t|sed -r "s/.*,(.*),.*$/\1/g")
+   for j in `cat $2 |sed -r "s/,.*//"`; do
+      echo -e "\e[32mconsolidating $t-$j\e[39m"
+      
+      $DIR/consolidate-capture.py -f ${t}-${j}.csvx -r ${resolution} -o ${t}_${j}_${resolution}.csvx
+      #$DIR/consolidate-capture.py -f ${t}-${j}.csvx -r ${resolution} -o ${t}_${j}_1H.csvx
+      echo -e "\e[32mconsolidated $t-$j\e[39m"
+   done
+   
 
 done
