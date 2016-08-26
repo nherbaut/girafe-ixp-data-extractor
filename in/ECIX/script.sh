ae2c0374a2734690630b1e071232da6fc94557fb
dbourasseau <dbourasseau@viotech.net>
2016-08-26 14:28:54 +0200

    add ECIX

--- in/ECIX/script.sh
+++ in/ECIX/script.sh
@@ -8,26 +8,20 @@ cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O $DI
 echo -e "\e[32mfor each config key"
 for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/\1/p'`; do
    echo -e "\e[32mworking with $t\e[39m"
-   #scaleY=$(cat $1|grep $t|sed -r "s/.*,(.*),.*$/\1/g")
    period=$(cat $1|grep $t|sed -r "s/.*,(.*)$/\1/g")
    echo -e "\e[32mperiod = $period\e[39m"
    for i in `ls $DIROUT/$t*.json`; do
       echo -e "\e[32mextracting $i\e[39m"   
-      #adate=$(ls $i|sed -r "s/^.*--(.*)\.json/\1/g")
-      
       for j in `cat $2 |sed -r "s/,.*//"`; do
 		echo $t-$j.csvx
-		$DIR/linx-data-extractor.py --file $i --ixp $j>> "${DIROUT}/$t-$j.csvx"
+		$DIR/ecix-data-extractor.py --file $i --ixp $j>> "${DIROUT}/$t-$j.csvx"
       done
    done
-   
    resolution=$(cat $1|grep $t|sed -r "s/.*,(.*),.*$/\1/g")
    for j in `cat $2 |sed -r "s/,.*//"`; do
-      echo -e "\e[32mconsolidating $t-$j\e[39m"
-      
+      echo -e "\e[32mConsolidating $t-$j\e[39m"
       $DIR/consolidate-capture.py -f "${DIROUT}/${t}-${j}.csvx" -r ${resolution} -o "${DIROUT}/${t}_${j}_${resolution}.csvx"
-      #$DIR/consolidate-capture.py -f ${t}-${j}.csvx -r ${resolution} -o ${t}_${j}_1H.csvx
-      echo -e "\e[32mconsolidated $t-$j\e[39m"
+      echo -e "\e[32mConsolidated $t-$j\e[39m"
    done
    
 
