e90da96ff5ee2b0c0529eb3340e6570dcebd3d67
dbourasseau <dbourasseau@viotech.net>
2016-08-26 18:42:20 +0200

    add script to exec all script && move out floder of France-ix

--- in/France-IX/script.sh
+++ in/France-IX/script.sh
@@ -1,8 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIROUT=$(echo $DIR | sed 's/\/in/\/out/')
 echo -e "\e[32mremoving old csvx file\e[39m"
-rm $DIR/*.csvx 2> /dev/null
+rm $DIROUT/*.csvx 2> /dev/null
 echo -e "\e[32mdownloading new png files\e[39m"
-cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O $DIR\/\1--$(date "+%Y-%m-%dT%H:%M:%S").png /p' | source /dev/stdin
+cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/wget --no-check-certificate "\2" -O $DIROUT\/\1--$(date "+%Y-%m-%dT%H:%M:%S").png /p' | source /dev/stdin
 
 echo -e "\e[32mfor each config key"
 for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/\1/p'`; do
@@ -10,14 +11,14 @@ for t in `cat $1 |sed -rn 's/^(.*),(.*),(.*),(.*)$/\1/p'`; do
    scaleY=$(cat $1|grep $t|sed -r "s/.*,(.*),.*$/\1/g")
    period=$(cat $1|grep $t|sed -r "s/.*,(.*)$/\1/g")
    echo -e "\e[32mscale = $scaleY         period = $period\e[39m"
-   for i in `ls $t*.png`; do
+   for i in `ls $DIROUT/$t*.png`; do
       echo -e "\e[32mextracting $i\e[39m"   
       adate=$(ls $i|sed -r "s/^.*--(.*)\.png/\1/g")
-      $DIR/franceix-data-extractor.py --file $i --date $adate --maxY $scaleY -p $period>> $t.csvx
+      $DIR/franceix-data-extractor.py --file $i --date $adate --maxY $scaleY -p $period>> $DIROUT/$t.csvx
    done
    echo -e "\e[32mconsolidating $t\e[39m"
-   $DIR/consolidate-capture.py -f ${t}.csvx -r 10T -o ${t}_10T.csvx
-   $DIR/consolidate-capture.py -f ${t}.csvx -r 1H -o ${t}_1H.csvx
+   $DIR/consolidate-capture.py -f "${DIROUT}/${t}.csvx" -r 10T -o "${DIROUT}/${t}_10T.csvx"
+   $DIR/consolidate-capture.py -f "${DIROUT}/${t}.csvx" -r 1H -o "${DIROUT}/${t}_1H.csvx"
    echo -e "\e[32mconsolidated $t\e[39m"
 
 done
---
+++ script.sh
@@ -0,0 +1,13 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo -e "\e[32mfor each start script\e[39m"
+for t in `ls $DIR/in`; do
+	if ls $DIR/in/$t/ixplist.csv 1> /dev/null 2>&1 ;then
+		$DIR/in$t/script.sh $DIR/in/$t/target.csv $DIR/in/$t/ixplist.csv
+	else
+		$DIR/in/$t/script.sh $DIR/in/$t/target.csv
+	fi
+
+done
+
+
