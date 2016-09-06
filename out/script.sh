92fdb04e1b80a51b9431dcc108e60e3c9ce46a1f
David Bourasseau <dbourasseau@viotech.net>
2016-09-06 09:45:02 +0000

    save of Tue Sep  6 09:45:02 UTC 2016

--- out/script.sh
+++ out/script.sh
@@ -2,5 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 git add -A
+date=$(date "+%Y-%m-%dT%H.%M.%S")
+git tag  "$date"
 git commit -a -m "save of $(date)"
+git push origin $date
 git push origin withdata
