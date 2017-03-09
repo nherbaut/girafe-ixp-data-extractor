3a7340f42d112513a14aec662e4f47a1d788e03c
David Bourasseau <david.bourasseau@gmail.com>
2017-03-09 11:23:46 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -3,10 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 git config --global user.email dbourasseau@viotech.net
 git checkout withdata
-git pull origin withdata
+git pull origin withdata --depth 1
 git add -A
 date=$(date "+%Y-%m-%dT%H.%M.%S")
 git commit -a -m "save of $(date)"
 git tag  "$date"
 git push origin $date
 git push origin withdata
+git gc
